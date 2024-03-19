851cb768b2093dddb8c292e563cc36257af9051e
stivik20 <151648222+stivik20@users.noreply.github.com>
2024-03-19 22:50:16 +0300

    Update srv1.sh

--- srv1/srv1.sh
+++ srv1/srv1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cp /root/otus_project/srv1/nginx.conf /etc/nginx/nginx.conf
+cp /root/otus_test/srv1/nginx.conf /etc/nginx/nginx.conf
 chmod 644 /etc/nginx/nginx.conf
-cp /root/otus_project/srv1/frontend.conf /etc/nginx/sites-available/frontend.conf
+cp /root/otus_test/srv1/frontend.conf /etc/nginx/sites-available/frontend.conf
 chmod 644 /etc/nginx/sites-available/frontend.conf
 rm /etc/nginx/sites-enabled/*
 ln -s /etc/nginx/sites-available/frontend.conf /etc/nginx/sites-enabled/frontend.conf
 systemctl reload nginx
-cp /root/otus_project/srv1/filebeat.yml /etc/filebeat/filebeat.yml
+cp /root/otus_test/srv1/filebeat.yml /etc/filebeat/filebeat.yml
 chmod 644 /etc/filebeat/filebeat.yml
 systemctl daemon-reload
 systemctl restart filebeat
