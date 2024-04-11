4cf9087403c554cb33da19c7b2ec3082d4f21d54
stivik20 <151648222+stivik20@users.noreply.github.com>
2024-04-11 23:45:57 +0300

    srv3.sh

--- srv3/srv3.sh
+++ srv3/srv3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cp /root/otus_project/srv3/000-default.conf /etc/apache2/sites-available/000-default.conf
+cp /root/otus_test/srv3/000-default.conf /etc/apache2/sites-available/000-default.conf
 systemctl reload apache2
-cp /root/otus_project/srv3/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+cp /root/otus_test/srv3/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 systemctl restart mysql
 
-backup_dir="/root/otus_project/mysql_backup/"
+backup_dir="/root/otus_test/mysql_backup/"
 for db_dir in $(ls $backup_dir); do
     db=$(basename $db_dir)
     for tbl_dir in $(ls $backup_dir/$db); do
@@ -17,20 +17,20 @@ done
 
 #DB_NAME="wordpress_db"
 #DB_USER="wordpress"
-#DB_PASSWORD="2Z%Zv3W93%"
+#DB_PASSWORD="Qq123456"
 #MYSQL_COMMANDS="CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
 #GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
 #FLUSH PRIVILEGES;"
 #mysql -e "$MYSQL_COMMANDS"
 
 DB_REPL_USER="repl"
-DB_REPL_PASSWORD="52v#Q&Xvvs"
+DB_REPL_PASSWORD="Qq123456"
 MYSQL_COMMANDS="STOP REPLICA;
-CHANGE REPLICATION SOURCE TO SOURCE_HOST='10.0.0.11', SOURCE_USER='$DB_REPL_USER', SOURCE_PASSWORD='$DB_REPL_PASSWORD', SOURCE_AUTO_POSITION = 1, GET_SOURCE_PUBLIC_KEY = 1;
+CHANGE REPLICATION SOURCE TO SOURCE_HOST='192.168.0.51', SOURCE_USER='$DB_REPL_USER', SOURCE_PASSWORD='$DB_REPL_PASSWORD', SOURCE_AUTO_POSITION = 1, GET_SOURCE_PUBLIC_KEY = 1;
 START REPLICA;"
 mysql -e "$MYSQL_COMMANDS"
 
-cp /root/otus_project/srv3/mysql.sh /root/mysql.sh
+cp /root/otus_test/srv3/mysql.sh /root/mysql.sh
 chmod +x /root/mysql.sh
 new_crontab="0 23 * * 6 /root/mysql.sh"
 current_crontab=$(crontab -l 2>/dev/null)
