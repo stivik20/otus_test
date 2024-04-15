d8c24a7c0074b5a72530c19f1ab60a0e67432624
stivik20 <151648222+stivik20@users.noreply.github.com>
2024-04-15 22:46:29 +0300

    srv4.sh

--- srv4/srv4.sh
+++ srv4/srv4.sh
@@ -2,8 +2,8 @@
 cp /root/otus_test/srv4/prometheus.yml /etc/prometheus/prometheus.yml
 chmod 644 /etc/prometheus/prometheus.yml
 #cp /root/otus_test/srv4/grafana.db /var/lib/grafana/
-chmod 640 /var/lib/grafana/grafana.db
-chown grafana:grafana /var/lib/grafana/grafana.db
+#chmod 640 /var/lib/grafana/grafana.db
+#chown grafana:grafana /var/lib/grafana/grafana.db
 systemctl daemon-reload
 systemctl restart prometheus
 systemctl restart grafana-server
