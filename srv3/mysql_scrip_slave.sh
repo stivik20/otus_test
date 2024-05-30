feb5be60c509758838dd440b7ebc4416cbe117c6
stivik20 <151648222+stivik20@users.noreply.github.com>
2024-05-30 23:02:01 +0300

    Update mysql_scrip_slave.sh

--- srv3/mysql_scrip_slave.sh
+++ srv3/mysql_scrip_slave.sh
@@ -2,23 +2,9 @@
 
 config_file="/etc/mysql/mysql.conf.d/mysqld.cnf"
 user="root"
-pass="qq"
-host="192.168.1.11"
+pass="Qq123456"
+host="192.168.0.51"
 user1="repl"
-
-# Добавляем новые строки в файл с помощью echo и оператора перенаправления >>
-if [ -f "$config_file" ]; then
-    # Добавляем новые строки в файл с помощью echo и оператора перенаправления >>
-    echo "server-id = 2" >> $config_file
-    echo "read-only = ON" >> $config_file
-    echo "gtid-mode=ON" >> $config_file
-    echo "enforce-gtid-consistency" >> $config_file
-    echo "log-replica-updates" >> $config_file
-else
-    echo "Файл $config_file не найден"
-fi
-service mysql restart
-echo "Конфиг MySQL настроен!"
 
 # Меняем источник репликации
 mysql -u$user -p$pass -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='$host', SOURCE_USER='$user1', SOURCE_PASSWORD='$pass', SOURCE_AUTO_POSITION = 1, GET_SOURCE_PUBLIC_KEY = 1;"
