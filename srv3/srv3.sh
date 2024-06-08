3a0e681300598af5416f408291c0a68cd8846acf
stivik20 <151648222+stivik20@users.noreply.github.com>
2024-06-09 00:08:16 +0300

    Update srv3.sh

--- srv3/srv3.sh
+++ srv3/srv3.sh
@@ -1,40 +1,22 @@
 #!/bin/bash
+
+a2enmod rewrite
+systemctl restart apache2
+rsync -av /root/otus_test/srv2/www/html/ /var/www/html
+chown -R www-data:www-data /var/www/html/
+chmod -R 755 /var/www/html/
+sudo rm /var/www/html/index.html
+
 cp /root/otus_test/srv3/000-default.conf /etc/apache2/sites-available/000-default.conf
 systemctl reload apache2
 cp /root/otus_test/srv3/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 systemctl restart mysql
 
-backup_dir="/root/otus_test/mysql_backup/"
-for db_dir in $(ls $backup_dir); do
-    db=$(basename $db_dir)
-    for tbl_dir in $(ls $backup_dir/$db); do
-        tbl=$(basename $tbl_dir)
-        tbl_backup="$backup_dir/$db/$tbl/$tbl.sql"
-        mysql -u root -e "CREATE DATABASE IF NOT EXISTS $db;"
-        mysql -u root $db < $tbl_backup
-    done
-done
+config_file="/etc/mysql/mysql.conf.d/mysqld.cnf"
+user="root"
+pass="Qq123456"
+host="192.168.0.51"
+user1="repl"
 
-DB_NAME="wordpress_db"
-DB_USER="root"
-DB_PASSWORD="Qq123456"
-MYSQL_COMMANDS="CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
-FLUSH PRIVILEGES;"
-mysql -e "$MYSQL_COMMANDS"
-
-DB_REPL_USER="repl"
-DB_REPL_PASSWORD="Qq123456"
-MYSQL_COMMANDS="STOP REPLICA;
-CHANGE REPLICATION SOURCE TO SOURCE_HOST='192.168.0.51', SOURCE_USER='$DB_REPL_USER', SOURCE_PASSWORD='$DB_REPL_PASSWORD', SOURCE_AUTO_POSITION = 1, GET_SOURCE_PUBLIC_KEY = 1;
-START REPLICA;"
-mysql -e "$MYSQL_COMMANDS"
-
-cp /root/otus_test/srv3/mysql.sh /root/mysql.sh
-chmod +x /root/mysql.sh
-new_crontab="0 23 * * 6 /root/mysql.sh"
-current_crontab=$(crontab -l 2>/dev/null)
-echo "$current_crontab" > temp_crontab
-echo "$new_crontab" >> temp_crontab
-crontab temp_crontab
-rm temp_crontab
+# Меняем источник репликации
+mysql -u$user -p$pass -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='$host', SOURCE_USER='$user1', SOURCE_PASSWORD='$pas>echo "Источник репликации успешно изменен"
