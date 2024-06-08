5e29ffbd8c5c2ce2bfb0b6a24b853a27e4f57454
stivik20 <151648222+stivik20@users.noreply.github.com>
2024-06-09 00:12:44 +0300

    Update srv3.sh

--- srv3/srv3.sh
+++ srv3/srv3.sh
@@ -19,4 +19,5 @@ host="192.168.0.51"
 user1="repl"
 
 # Меняем источник репликации
-mysql -u$user -p$pass -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='$host', SOURCE_USER='$user1', SOURCE_PASSWORD='$pas>echo "Источник репликации успешно изменен"
+mysql -u$user -p$pass -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='$host', SOURCE_USER='$user1', SOURCE_PASSWORD='$pass', SOURCE_AUTO_POSITION = 1, GET_SOURCE_PUBLIC_KEY = 1;"
+echo "Источник репликации успешно изменен"
