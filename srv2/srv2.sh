add8884b19b6c00a9e289888914ed0d184a9de16
stivik20 <151648222+stivik20@users.noreply.github.com>
2024-06-08 23:44:33 +0300

    Update srv2.sh

--- srv2/srv2.sh
+++ srv2/srv2.sh
@@ -1,42 +1,58 @@
 #!/bin/bash
-rsync -av /root/otus_test/srv2/www/html /var/www/html
-#chown -R www-data:www-data /var/www/html/
-#chmod -R 755 /var/www/html/
+a2enmod rewrite
+systemctl restart apache2
+rsync -av /root/otus_test/srv2/www/html/ /var/www/html
+chown -R www-data:www-data /var/www/html/
+chmod -R 755 /var/www/html/
+sudo rm /var/www/html/index.html
 cp /root/otus_test/srv2/000-default.conf /etc/apache2/sites-available/000-default.conf
 systemctl reload apache2
 cp /root/otus_test/srv2/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 systemctl restart mysql
 
-#backup_dir="/root/otus_test/mysql_backup/"
-#for db_dir in $(ls $backup_dir); do
-#    db=$(basename $db_dir)
-#    for tbl_dir in $(ls $backup_dir/$db); do
-#        tbl=$(basename $tbl_dir)
-#        tbl_backup="$backup_dir/$db/$tbl/$tbl.sql"
-#        mysql -u root -e "CREATE DATABASE IF NOT EXISTS $db;"
-#        mysql -u root $db < $tbl_backup
-#    done
-#done
+
+# Настройки подключения к MySQL
 user="root"
 pass="Qq123456"
+BACKUP_DIR="/root/otus_test/srv2/"
 
-DB_NAME="wp_database"
-DB_USER="wp_user"
-DB_PASSWORD="Qq123456"
-DB_REPL_USER="repl"
-DB_REPL_PASSWORD="Qq123456"
-mysql -u$user -p$pass -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD'";
-mysql -u$user -p$pass -e GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
-mysql -u$user -p$pass -e FLUSH PRIVILEGES;
-mysql -u$user -p$pass -e CREATE USER '$DB_REPL_USER'@'%' IDENTIFIED BY '$DB_REPL_PASSWORD';
-mysql -u$user -p$pass -e GRANT REPLICATION SLAVE ON *.* TO '$DB_REPL_USER'@'%';
-mysql -u$user -p$pass -e FLUSH PRIVILEGES;"
-mysql -e "$MYSQL_COMMANDS"
+mysql -u$user -p$pass -e "CREATE DATABASE wp_database;"
+mysql -u$user -p$pass -e "CREATE USER 'stivik'@'localhost' IDENTIFIED BY 'Qq123456';"
+mysql -u$user -p$pass -e "GRANT ALL PRIVILEGES ON wp_database.* TO 'stivik'@'localhost';"
+mysql -u$user -p$pass -e "FLUSH PRIVILEGES;"
 
-#unison -batch /var/www ssh://192.168.0.52//var/www -owner -group
-#new_crontab="* * * * * unison -batch /var/www ssh://192.168.0.52//var/www &> /dev/null"
-#current_crontab=$(crontab -l 2>/dev/null)
-#echo "$current_crontab" > temp_crontab
-#echo "$new_crontab" >> temp_crontab
-#crontab temp_crontab
-#rm temp_crontab
+cd otus_test/srv2/
+mysql -u root -p wp_database < dump.sql
+cd
+
+# Создаём пользователя для реплики
+mysql -u$user -p$pass -e "CREATE USER 'repl'@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'Qq123456';"
+# Даём ему права на репликацию
+
+# Цикл по базам данных
+for DB_NAME in $DATABASES; do
+    # Создание имени файла для резервной копии в формате "название_базы_данных.sql"
+    FILENAME="${DB_NAME}_backup_mysql.sql"
+    # Создание полного пути к файлу резервной копии
+    BACKUP_PATH="${BACKUP_DIR}/${FILENAME}"
+    # Создание резервной копии базы данных
+    mysqldump -u$user -p$pass --set-gtid-purged=OFF --source-data=1 --add-drop-table --databases --add-locks --create-o>    # Проверка успешности выполнения команды mysqldump
+    if [ $? -eq 0 ]; then
+        echo "Резервная копия Б/Д $DB_NAME создана успешно. Путь к Б/Д: $BACKUP_PATH"
+    else
+        echo "Возникла ошибка при создании резервной копии базы данных $DB_NAME"
+    fi
+done
+
+mysql -u$user -p$pass -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';"
+echo "Пользователь repl добавлен в MySQL и получил права на репликацию."
+# Устанавливаем пароль
+mysql -u$user -p$pass -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'caching_sha2_password' BY 'Qq123456';"
+echo "Для пользователь repl в MySQL, установлен пароль Qq123456"
+echo "Сервис Mysql готов к работе!"
+
+# Получение списка баз данных
+DATABASES=$(mysql -u$user -p$pass -e "SHOW DATABASES" | grep -Ev "(Database|information_schema|performance_schema|mysql>
+
+# Создание директории для резервных копий
+mkdir -p $BACKUP_DIR
